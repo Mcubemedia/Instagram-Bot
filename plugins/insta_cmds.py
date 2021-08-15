40e25c6eb2d69f40e99ab542d79207bae6106e19
Mcubemedia <88975183+Mcubemedia@users.noreply.github.com>
2021-08-15 20:18:49 +0530

    Update insta_cmds.py

--- plugins/insta_cmds.py
+++ plugins/insta_cmds.py
@@ -40,16 +40,16 @@ insta = Config.L
 buttons=InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-            InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+            InlineKeyboardButton("⚡️𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥", url='https://t.me/mcubemediaofficial'),
+            InlineKeyboardButton("👥𝐒𝐮𝐩𝐩𝐨𝐫𝐭 𝐆𝐫𝐨𝐮𝐩", url="https://t.me/mcubemediasupport")
         ],
         [
-            InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-            InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
+            InlineKeyboardButton("🔗Source Code", url="https://t.me/smplmcube/7"),
+            InlineKeyboardButton("🧩Deploy Own Bot", url="https://t.me/smplmcube/7")
         ],
         [
             InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin"),
-            InlineKeyboardButton("⚙️Update Channel", url="https://t.me/subin_works")
+            InlineKeyboardButton("✏ 𝙴𝚍𝚒𝚝𝚘𝚛", url="https://t.me/rashmikamandannaofficial1")
         ]
 					
     ]

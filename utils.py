49c4e93b8d6d01ff7442b01b120bea164e96606b
Mcubemedia <88975183+Mcubemedia@users.noreply.github.com>
2021-08-15 19:43:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -192,12 +192,12 @@ async def upload(m, bot, chat_id, dir):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122")
+					InlineKeyboardButton("Main Channel", url='https://t.me/mcubemediaofficial'),
+					InlineKeyboardButton("Main Group", url="https://t.me/mcubemediasupport")
 				],
 				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
-                    InlineKeyboardButton("⚡️Update Channel", url="https://t.me/subin_works")
+					InlineKeyboardButton("🔗Source Code", url="https://t.me/smplmcube/7"),
+                    InlineKeyboardButton("Developer", url="https://t.me/subinps")
 				]
 			]
 			)
